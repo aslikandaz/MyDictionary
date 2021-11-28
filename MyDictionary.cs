e10df34367b78ee7df41b4cea43af379c99aece7
aslikandaz <79531642+aslikandaz@users.noreply.github.com>
2021-11-28 16:08:49 +0300

    .

---
+++ MyDictionary.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdevDictionary
+{
+    class MyDictionary<K, V>
+    {
+        K[] arrayKey;
+        V[] arrayValue;
+
+        public MyDictionary()
+        {
+            arrayKey = new K[0];
+            arrayValue = new V[0];
+        }
+
+        public void Add(K Key, V Value)
+        {
+            K[] tempAnahtar = arrayKey;
+            V[] tempDeğer = arrayValue;
+
+            if (arrayValue.Contains(Value)==true || arrayKey.Contains(Key)==true) // contains dizi içindeki elemanlarda arama yapar ve true ya da false değer döner
+            {
+                Console.WriteLine("bu anahtar ya da değer dizide mevcut");
+                // dizide verilen değer ya da anahtar varsa eklemeyerek ekrana bilgi basar
+            }
+            else // dizide değer ve anahtar bulunmazsa ekleme işlemini yapar
+            {
+                arrayKey = new K[tempAnahtar.Length + 1];
+                arrayValue = new V[tempDeğer.Length + 1];
+
+                for (int i = 0; i < tempDeğer.Length; i++)
+                {
+                    arrayKey[i] = tempAnahtar[i];
+                    arrayValue[i] = tempDeğer[i];
+                }
+                arrayKey[arrayKey.Length - 1] = Key;
+                arrayValue[arrayValue.Length - 1] = Value;
+            }
+           
+        }
+
+        // dizideki değer sayısını döner
+        public int Length
+        {
+            get { return arrayValue.Length; }
+        }
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OdevDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "aslı");
+            myDictionary.Add(2, "halime");
+            myDictionary.Add(3, "ayşe");
+            myDictionary.Add(4, "fatma");
+           
+
+            Console.WriteLine(myDictionary.Length);
+        }
+    }
+}
